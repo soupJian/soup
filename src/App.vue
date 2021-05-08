d51323f9fec77b7afd88cf23d4c0fc370021fc3f
soupjian <soupjian@163.com>
2021-05-08 17:42:18 +0800

    feat: 用户登陆过后，下次进来直接进入index界面

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,17 @@
 import { onMounted } from 'vue'
 import { useStore } from 'vuex'
 import $socket from '@/util/socket'
+import { useRouter } from 'vue-router'
 export default {
   setup(){
     const store = useStore()
+    const router = useRouter()
     const user = store.state.user
     // 用户已经登陆过，下次进来自动进行socket连接
     if(user.id){
       onMounted(()=>{
         $socket.emit('online',user.id)
+        router.replace('/index')
       })
     }
   }
